7b5ef6f261cc7065feecf5e32371189a41555352
Chris Yang <kimisme9386@gmail.com>
2021-04-27 11:24:24 +0800

    feat: Add lambda to implement badage, github commit status and slack web hook

---
+++ lambda/codepipeline-event/src/codepipelineEventLambda.ts
@@ -0,0 +1,173 @@
+import {
+  CodePipelineClient,
+  GetPipelineExecutionCommand,
+} from '@aws-sdk/client-codepipeline';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { Callback, CodePipelineCloudWatchEvent, Context } from 'aws-lambda';
+import { default as axios } from 'axios';
+import url from 'url';
+
+enum CodePipelineState {
+  STARTED = 'STARTED',
+  RESUMED = 'RESUMED',
+  CANCELED = 'CANCELED',
+  FAILED = 'FAILED',
+  SUCCEEDED = 'SUCCEEDED',
+  SUPERSEDED = 'SUPERSEDED',
+}
+
+interface SourceActionData {
+  owner: string;
+  repository: string;
+  sha: string;
+}
+
+const CodePipelineFailState = [
+  CodePipelineState.CANCELED as string,
+  CodePipelineState.FAILED as string,
+  CodePipelineState.SUCCEEDED as string,
+];
+
+export const handler = async (
+  event: CodePipelineCloudWatchEvent,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  context: Context,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  callback: Callback
+): Promise<void> => {
+  console.info('Debug event\n' + JSON.stringify(event, null, 2));
+  const state = event.detail.state;
+  const subject = `project: ${event.detail.pipeline} \n ${event['detail-type']}: ${state}`;
+  const webhookUrl = (process.env.SLACK_WEBHOOK_URL as string) ?? '';
+  const badgeBucket = process.env.BADGE_BUCKET_NAME as string;
+  const badgeBucketImageKeyName = process.env
+    .BADGE_BUCKET_IMAGE_KEY_NAME as string;
+  const passingSvgUrl =
+    'https://img.shields.io/badge/AWS%20CodePipeline-passing-green.svg';
+  const failSvgUrl =
+    'https://img.shields.io/badge/AWS%20CodePipeline-fail-red.svg';
+  const executionId = event.detail['execution-id'];
+  const codePipelineName = process.env.CODE_PIPELINE_NAME as string;
+  const githubPersonalToken =
+    (process.env.GITHUB_PERSONAL_TOKEN as string) ?? '';
+  const stageTitle = process.env.STAGE ? `${process.env.STAGE}: ` : '';
+
+  if (webhookUrl) {
+    const respData = await axios
+      .create({
+        headers: { 'Context-Type': 'application/json' },
+      })
+      .post(webhookUrl, { text: `${stageTitle}${subject}` });
+    console.log(`webhookUrl response:\n ${respData}`);
+  }
+
+  let imageUrl: string | null = null;
+  if (state == CodePipelineState.SUCCEEDED) {
+    imageUrl = passingSvgUrl;
+  } else if (CodePipelineFailState.includes(state)) {
+    imageUrl = failSvgUrl;
+  }
+
+  console.log(`debug badge update image: ${imageUrl}`);
+  if (imageUrl) {
+    const imageResp = await axios.get(imageUrl, {
+      responseType: 'arraybuffer',
+    });
+
+    const s3 = new S3Client({ region: 'ap-northeast-1' });
+    await s3.send(
+      new PutObjectCommand({
+        Bucket: badgeBucket,
+        Key: badgeBucketImageKeyName,
+        Body: Buffer.from(imageResp.data),
+        ContentType: 'image/svg+xml',
+        CacheControl: 'cache-control: no-cache',
+        Expires: new Date(Date.now()),
+      })
+    );
+  }
+
+  const sourceActionData = await getPipelineSourceActionData(
+    executionId,
+    codePipelineName
+  );
+
+  let sourceActionState: string | null = null;
+
+  switch (state) {
+    case CodePipelineState.STARTED:
+    case CodePipelineState.RESUMED:
+    case CodePipelineState.SUPERSEDED:
+      sourceActionState = 'pending';
+      break;
+
+    case CodePipelineState.SUCCEEDED:
+      sourceActionState = 'success';
+      break;
+
+    case CodePipelineState.CANCELED:
+    case CodePipelineState.FAILED:
+      sourceActionState = 'error';
+      break;
+  }
+
+  console.log(`debug state: ${state}`);
+  console.log(`debug sourceActionData: ${JSON.stringify(sourceActionData)}`);
+  if (githubPersonalToken && sourceActionData && sourceActionState) {
+    console.log(
+      `sourceActionCommitStatusUrl:\n https://api.github.com/repos/${sourceActionData?.owner}/${sourceActionData?.repository}/statuses/${sourceActionData?.sha}`
+    );
+    const respSourceActionData = await axios
+      .create({
+        headers: {
+          'Context-Type': 'application/json',
+          Authorization: `token ${githubPersonalToken}`,
+        },
+      })
+      .post(
+        `https://api.github.com/repos/${sourceActionData?.owner}/${sourceActionData?.repository}/statuses/${sourceActionData?.sha}`,
+        {
+          state: sourceActionState,
+          target_url: `https://ap-northeast-1.console.aws.amazon.com/codesuite/codepipeline/pipelines/${codePipelineName}/view`,
+          context: 'continuous-integration/codepipeline',
+          description: `Build ${sourceActionState}`,
+        }
+      );
+    console.log(respSourceActionData);
+  }
+};
+
+const getPipelineSourceActionData = async (
+  executionId: string,
+  pipelineName: string
+): Promise<SourceActionData | null> => {
+  const client = new CodePipelineClient({
+    region: 'ap-northeast-1',
+  });
+  const result = await client.send(
+    new GetPipelineExecutionCommand({
+      pipelineExecutionId: executionId,
+      pipelineName: pipelineName,
+    })
+  );
+  console.log(`pipeline data:\n ${JSON.stringify(result)}`);
+  const artifactRevision = result.pipelineExecution?.artifactRevisions
+    ? result.pipelineExecution?.artifactRevisions[0]
+    : null;
+
+  if (artifactRevision) {
+    const revisionURL = artifactRevision.revisionUrl;
+    const sha = artifactRevision.revisionId;
+    const fullRepositoryId = new url.URL(
+      revisionURL as string
+    ).searchParams.get('FullRepositoryId') as string;
+
+    return {
+      owner: fullRepositoryId ? fullRepositoryId.split('/')[0] : '',
+      repository: fullRepositoryId ? fullRepositoryId.split('/')[1] : '',
+      sha: sha ? sha : '',
+    };
+  }
+
+  return null;
+};
---
+++ src/main.ts
@@ -0,0 +1,129 @@
+import * as codePipeline from '@aws-cdk/aws-codepipeline';
+import * as targets from '@aws-cdk/aws-events-targets';
+import * as iam from '@aws-cdk/aws-iam';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as cdk from '@aws-cdk/core';
+import * as path from 'path';
+
+interface CodePipelineBadgeProps {
+  /**
+   * AWS CodePipeline arn
+   */
+  pipelineArn: string;
+  /**
+   * GitHub personal token
+   */
+  gitHubToken?: string;
+  notification?: Notification;
+}
+
+interface Notification {
+  /**
+   * Prefix title for slack message
+   */
+  stageName?: string;
+  /**
+   * Slack webhook url
+   */
+  slackWebHookUrl?: string;
+}
+
+export class CodePipelineBadge extends cdk.Construct {
+  constructor(scope: cdk.Construct, id: string, props: CodePipelineBadgeProps) {
+    super(scope, id);
+
+    const pipeline = codePipeline.Pipeline.fromPipelineArn(
+      this,
+      'CodePipeline',
+      props.pipelineArn
+    );
+
+    const gitHubToken = props.gitHubToken
+      ? cdk.SecretValue.secretsManager(props.gitHubToken).toString()
+      : '';
+
+    const targetLambda = this.createCodePipelineEventLambdaFunction(
+      pipeline.pipelineName,
+      gitHubToken,
+      props?.notification?.stageName,
+      props?.notification?.slackWebHookUrl
+    );
+
+    pipeline.onStateChange('CodePipelineChange', {
+      eventPattern: {
+        source: ['aws.codepipeline'],
+        detailType: ['CodePipeline Pipeline Execution State Change'],
+      },
+      target: new targets.LambdaFunction(targetLambda),
+    });
+  }
+
+  private createCodePipelineEventLambdaFunction(
+    codePipelineName: string,
+    gitHubToken: string,
+    stage: string | undefined,
+    slackWebhookURL: string | undefined
+  ): lambda.Function {
+    const badgeBucket = new s3.Bucket(this, 'BadgeBucket', {
+      publicReadAccess: true,
+    });
+
+    const stageKeyName = stage ? `${stage}-` : '';
+    const badgeBucketImageKeyName = `${stageKeyName}latest-build.svg`;
+
+    const lambdaFunc = new lambda.Function(this, 'CodepipelineEventLambda', {
+      code: lambda.Code.fromAsset(
+        path.join(__dirname, '../lambda/codepipeline-event'),
+        {
+          bundling: {
+            user: 'root',
+            image: lambda.Runtime.NODEJS_14_X.bundlingImage,
+            command: [
+              'bash',
+              '-c',
+              [
+                'npm install',
+                'npm run build',
+                'cp -r /asset-input/dist /asset-output/',
+                'npm install --only=production',
+                'cp -a /asset-input/node_modules /asset-output/',
+              ].join(' && '),
+            ],
+          },
+        }
+      ),
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'dist/codepipelineEventLambda.handler',
+      environment: {
+        STAGE: stage ?? '',
+        SLACK_WEBHOOK_URL: slackWebhookURL ?? '',
+        BADGE_BUCKET_NAME: badgeBucket.bucketName,
+        BADGE_BUCKET_IMAGE_KEY_NAME: badgeBucketImageKeyName,
+        CODE_PIPELINE_NAME: codePipelineName,
+        GITHUB_PERSONAL_TOKEN: gitHubToken,
+      },
+    });
+
+    badgeBucket.grantReadWrite(lambdaFunc);
+
+    new cdk.CfnOutput(this, 'badgeMarkdownLink', {
+      value: `[![Build Status](https://${badgeBucket.bucketName}.s3-ap-northeast-1.amazonaws.com/${badgeBucketImageKeyName}#1)](https://ap-northeast-1.console.aws.amazon.com/codesuite/codepipeline/pipelines/${codePipelineName}/view)`,
+    });
+
+    lambdaFunc.role?.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName(
+        'AWSCodePipeline_ReadOnlyAccess'
+      )
+    );
+
+    this.tagResource(lambdaFunc);
+
+    return lambdaFunc;
+  }
+
+  private tagResource(scope: cdk.Construct): void {
+    cdk.Tags.of(scope).add('CDK-CfnStackId', cdk.Aws.STACK_ID);
+    cdk.Tags.of(scope).add('CDK-CfnStackName', cdk.Aws.STACK_NAME);
+  }
+}
