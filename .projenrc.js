68ae183f39a59cc066a98a628dd9d40c500d7745
Chris Yang <kimisme9386@gmail.com>
2021-12-24 07:01:51 +0000

    fix: use 0.x version for cdkv1

--- .projenrc.js
+++ .projenrc.js
@@ -13,7 +13,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   majorVersion: 2,
   defaultReleaseBranch: 'main',
   releaseBranches: {
-    cdkv1: { npmDistTag: 'cdkv1', majorVersion: 1 },
+    cdkv1: { npmDistTag: 'cdkv1', majorVersion: 0 },
   },
   keywords: ['aws', 'cdk', 'codepipeline', 'badge', 'notification'],
   jsiiFqn: 'projen.AwsCdkConstructLibrary',
